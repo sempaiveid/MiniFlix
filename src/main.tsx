61ba1bb35d37d3274ad4bab7bfe04e8abf3673ed
sempaiveid <vladuk640@gmail.com>
2026-01-18 01:58:45 +0200

    test husky main

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import "@app/styles/global.css"
-import { App } from "@app"
-const   a=1
-console.log(a)   
+import "@app/styles/global.css";
+import { App } from "@app";
+const a = 1;
+console.log(a);
 
 createRoot(document.getElementById("root")!).render(
-  
   <StrictMode>
-    <App />                
+    <App />
   </StrictMode>,
 );
