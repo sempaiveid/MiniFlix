d87941cced81ab19ce458813c834f7b06700fd1e
sempaiveid <vladuk640@gmail.com>
2026-01-18 01:51:57 +0200

    chore: normalize line endings

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from "react-dom/client"
 
 import "@app/styles/global.css"
 import { App } from "@app"
+const   a=1
+console.log(a)   
 
 createRoot(document.getElementById("root")!).render(
+  
   <StrictMode>
     <App />                
   </StrictMode>,
