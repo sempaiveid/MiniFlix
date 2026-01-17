2c6edbf5309aad853041bda8eb018d18ddedadeb
sempaiveid <vladuk640@gmail.com>
2026-01-18 01:47:26 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
-import "@app/styles/global.css";
-import { App } from "@app";
+import "@app/styles/global.css"
+import { App } from "@app"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <App />                
   </StrictMode>,
 );
